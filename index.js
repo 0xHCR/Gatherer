60a5a48706234509bf5dd2412ae83b7eca4f64ef
0xHCR <0xHCR@pm.me>
2021-04-12 21:49:34 +0200

    initial GQL queries

--- index.js
+++ index.js
@@ -1,6 +1,95 @@
-const fetch = require('node-fetch');
 const dotenv = require('dotenv');
+
+const gql = require('graphql-tag')
+const { GraphQLWrapper } = require('@aragon/connect-thegraph')
 
 dotenv.config();
 
-console.log(`Addy to be checked is: ${process.env.address}`);
+/* saving LP queries for later
+const USER_POOLS = gql`
+  mints(orderBy: timestamp, orderDirection: desc, where: { to: "${process.env.ADDRESS}" }) {
+    id
+    transaction {
+      id
+        timestamp
+    }
+    pair {
+      id
+      token0 {
+        id
+        symbol
+      }
+      token1 {
+        id
+        symbol
+      }
+    }
+    to
+    liquidity
+    amount0
+    amount1
+    amountUSD
+  }
+  burns(orderBy: timestamp, orderDirection: desc, where: { sender: "${process.env.ADDRESS}" }) {
+    id
+    transaction {
+      id
+      timestamp
+    }
+    pair {
+      id
+      token0 {
+        symbol
+      }
+      token1 {
+        symbol
+      }
+    }
+    sender
+    to
+    liquidity
+    amount0
+    amount1
+    amountUSD
+  }`*/
+
+const USER_TRANSACTIONS = gql`
+    {
+      swaps(orderBy: timestamp, orderDirection: desc, where: { to: "${process.env.ADDRESS}" }) {
+      id
+      transaction {
+        id
+        timestamp
+      }
+      pair {
+        token0 {
+          symbol
+        }
+        token1 {
+          symbol
+        }
+      }
+      amount0In
+      amount0Out
+      amount1In
+      amount1Out
+      amountUSD
+      to
+    }
+  }
+`
+
+async function getTransactions(message) {
+  const graphqlClient = new GraphQLWrapper(process.env.SUBGRAPH_URI)
+  const result = await graphqlClient.performQuery(USER_TRANSACTIONS)
+
+  if (!result.data) {
+    console.log('shit');
+    return
+  }
+
+  console.log(result.data);
+
+}
+
+getTransactions();
